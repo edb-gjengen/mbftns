9adfb550ae228971cf0804429c446925bb53a580
Nikolai Kristiansen <nikolaik@gmail.com>
2012-02-02 23:27:36 +0100

    Fixes PDF encoding

--- main/pdf.py
+++ main/pdf.py
@@ -21,7 +21,8 @@ def render_to_pdf(template_src, context_dict, context_instance):
     pdf = pisa.pisaDocument(
             StringIO.StringIO(html.encode("UTF-8")),
             dest=result,
-            link_callback=fetch_resources)
+            link_callback=fetch_resources,
+            encoding='UTF-8')
     if not pdf.err:
         return HttpResponse(result.getvalue(), mimetype='application/pdf')
     return HttpResponse('We had some errors<pre>%s</pre>' % escape(html))
